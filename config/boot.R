1aa942caa19b80301b39848dc680caf55134437f
RobertZK <technoguyrob@gmail.com>
2015-07-24 22:00:56 -0500

    Temporary fixes to base bootstrapping

--- config/boot.R
+++ config/boot.R
@@ -9,8 +9,7 @@ director$register_parser("lib/controllers",
 # Fetch the preprocessor and parser for the routes.
 routes <- director$resource("lib/controllers/routes")
 
-# Register the routes preprocesser and parser on the routes file explicitly.
-director$register_preprocessor("config/routes", routes$preprocessor)
+# Register the routes parser on the routes file explicitly.
 director$register_parser("config/routes", routes$parser)
 
 # Now load the routes, which will bring in all the other controllers.
--- config/engines.R
+++ config/engines.R
@@ -1,13 +1,16 @@
 # When the base engine is included in another engine, auto-source config/routes.
 .onAttach <- function(parent_engine) {
   # `director` is the base engine object.
+
+  parent_engine$register_parser("lib/controllers",
+    director$resource("lib/controllers/controller")$parser, overwrite = TRUE)
+
   routes <- director$resource("lib/controllers/routes")
 
-  parent_engine$register_preprocessor("config/routes", routes$preprocessor, overwrite = TRUE)
   parent_engine$register_parser("config/routes", routes$parser, overwrite = TRUE)
 
   # TODO: (RK) Fix this hack using a proper helper resource.
-  environment(routes$preprocessor)$mount(parent_engine)(director)
+  #environment(routes$preprocessor)$mount(parent_engine)(director)
 
   parent_engine$resource("config/routes")
 }
