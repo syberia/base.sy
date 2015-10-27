a73b36278e7a0ecde9a527688288ffbf714ae6f3
RobertZK <technoguyrob@gmail.com>
2015-10-27 14:21:30 -0700

    Update boot file to check for bootstrapping.

--- config/boot.R
+++ config/boot.R
@@ -1,17 +1,19 @@
 # It wrought itself from the void, defying the laws which govern
 # (1) existence and (2) other such matters.
 
-# Register the controllers controller explicitly.
-director$register_parser("lib/controllers",
-  director$resource("lib/controllers/controller")
-)
+if (!isTRUE(director$cache_get("bootstrapped"))) {
+  # Register the controllers controller explicitly.
+  director$register_parser("lib/controllers",
+    director$resource("lib/controllers/controller")
+  )
 
-# Fetch the preprocessor and parser for the routes.
-routes <- director$resource("lib/controllers/routes")
+  # Fetch the preprocessor and parser for the routes.
+  routes <- director$resource("lib/controllers/routes")
 
-# Register the routes parser on the routes file explicitly.
-director$register_parser("config/routes", routes$parser)
+  # Register the routes parser on the routes file explicitly.
+  director$register_parser("config/routes", routes$parser)
 
-# Now load the routes, which will bring in all the other controllers.
-director$resource("config/routes")
+  # Now load the routes, which will bring in all the other controllers.
+  director$resource("config/routes")
+}
 
