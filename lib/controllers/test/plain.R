1b846501dbc64b06bf98859415e8bdfcd787626c
RobertZK <technoguyrob@gmail.com>
2015-12-22 09:36:43 -0600

    Add testthat integration

--- lib/controllers/test/plain.R
+++ lib/controllers/test/plain.R
@@ -1,5 +1,5 @@
 # The default preprocessor for syberia test environments.
-preprocessor <- function(resource, director, source_env, source) {
+preprocessor <- function(resource, director, source_env, source, filename, args) {
   tested_resource <- gsub("^test\\/", "", resource)
 
   # Provide access to the director for people with hardcore test setup
@@ -26,7 +26,15 @@ preprocessor <- function(resource, director, source_env, source) {
     }
   }
 
-  source()
+  test_args <- list(filename, env = source_env)
+
+  if (is.element("reporter", names(args))) {
+    test_args$reporter <- args$reporter
+    test_args$start_end_reporter <- FALSE
+  }
+
+  testthat::context(tested_resource)
+  do.call(testthat::test_file, test_args)
 }
 
 # No parser.
